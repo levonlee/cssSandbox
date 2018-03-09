e3c01e6476abd05138e40e6101972137703dec04
Li Li <5024766+levonlee@users.noreply.github.com>
2018-03-09 12:28:16 -0500

    postcss

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
+var gulp = require('gulp');
+
 gulp.task('autoprefixer', function () {
   var postcss = require('gulp-postcss');
   var sourcemaps = require('gulp-sourcemaps');
   var rename = require('gulp-rename');
   var autoprefixer = require('autoprefixer');
 
-  return gulp.src('*-gulp.css', {base: '.'})
+  return gulp.src('**/*-gulp.css', {base: '.'})
     .pipe(sourcemaps.init())
     .pipe(postcss([autoprefixer()]))
     .pipe(sourcemaps.write('.'))
