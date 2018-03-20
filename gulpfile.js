6c9e5d177a891ae5700f2a9bbf71a447def87740
Li Li <5024766+levonlee@users.noreply.github.com>
2018-03-20 12:38:17 -0400

    gulp-responsive

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp');
+var gResponsive = require('gulp-responsive');
 
 gulp.task('autoprefixer', function () {
   var postcss = require('gulp-postcss');
@@ -14,4 +15,69 @@ gulp.task('autoprefixer', function () {
       path.basename += '-build';
     }))
     .pipe(gulp.dest('.'));
+});
+
+gulp.task('img-resize', function () {
+  return gulp.src('src/*.{jpg,png}')
+    .pipe(gResponsive({
+      '*.jpg': {
+        // Resize all JPG images to 200 pixels wide
+        width: 200,
+      },
+      '*.png': {
+        // Resize all PNG images to 50% of original pixels wide
+        width: '50%',
+      },
+      // Resize all images to 100 pixels wide and add suffix -thumbnail
+      '*': {
+        width: 100,
+        rename: { suffix: '-thumbnail' },
+      },
+    }, {
+      // Global configuration for all images
+      // The output quality for JPEG, WebP and TIFF output formats
+      quality: 70,
+      // Use progressive (interlace) scan for JPEG and PNG output
+      progressive: true,
+      // Zlib compression level of PNG output format
+      compressionLevel: 6,
+      // Strip all metadata
+      withMetadata: false,
+    }))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('img-responsive', function () {
+  return gulp.src('src/*.{jpg,png}')
+    .pipe(gResponsive({
+      // Resize all JPG images to three different sizes: 200, 500, and 630 pixels
+      '*.jpg': [{
+        width: 70,
+        rename: { suffix: '-70s' },
+      }, {
+        width: 70 * 2,
+        rename: { suffix: '-70s@2x' },
+      }],
+      // Resize all PNG images to be retina ready
+      '*.png': [{
+        width: 250,
+      }, {
+        width: 250 * 2,
+        rename: { suffix: '@2x' },
+      }],
+      /*
+      '*.jpg': [{
+        // either one of width or rename can be ignored
+      }],
+      */
+    }, {
+      // Global configuration for all images
+      // The output quality for JPEG, WebP and TIFF output formats
+      quality: 100,
+      // Use progressive (interlace) scan for JPEG and PNG output
+      progressive: true,
+      // Strip all metadata
+      withMetadata: false,
+    }))
+    .pipe(gulp.dest('dist'));
 });
