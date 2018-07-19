c7aaa014d230b9119aff840949179497fc825b00
Li Li <levonlee@gmail.com>
2018-07-19 11:35:07 -0400

    svg text type custom text

--- svg/js/svg-text.js
+++ svg/js/svg-text.js
@@ -8,34 +8,49 @@
 
   ns.debugState = true;
 
-  ns.debug = function(msg) {
+  ns.debug = function() {
     if (ns.debugState) {
-      console.log(msg);
+      for (var i=0; i<arguments.length; i++) {
+        console.log(arguments[i]);
+      }
     }
   }
 
   ns.setViewbox = function($el, $v) {
-    //$el.removeAttr('viewBox'); // Don't remove and change.. just change it
+    //$el.removeAttr('viewBox'); // Hard lesson.. Don't remove and change.. just set it
     $el[0].setAttribute('viewBox', '0 0 ' + $v + ' 19');
   }
 
-  ns.changeText = function(event) {
-    var $target = $(event.target),
-      oldLength = 6, // 'Fit Me' length = 6
+  ns.cbCustomText = function(e) {
+    var $target = $(e.target);
+    ns.changeText($target.val())
+  }
+
+  ns.cbSelectionText = function(e) {
+    var $elCustomText = $('#lili-custom-text'),
+        $target = $(e.target),
+        val = $target.text();
+    $elCustomText.val(val);
+    ns.changeText(val);
+  }
+
+  ns.changeText = function(val) {
+    var oldLength = 6, // 'Fit Me' length = 6
       oldSvgViewBox = 45, // <svg viewBox="0 0 45 18">
       perLetter =  oldSvgViewBox / oldLength,
-      val = $target.text(),
       valSvgViewBox = val.length * perLetter,
       $elContainer = $('.lili-text-container'),
       $elSvg = $('.lili-text-container svg'),
-      $elSvgText = $('.lili-text-container svg text');
+      $elSvgText = $('.lili-text-container svg text'),
+      $elCustomText = $('#lili-custom-text');
 
     $elContainer.removeClass('lili-text-container-transform');
 
+    //$elCustomText.val(val);
     $elSvgText.text(val); // .html(val) does not work on IE11
     ns.setViewbox($elSvg,valSvgViewBox);
 
-    console.log('per letter length: ' + perLetter,
+    ns.debug('per letter length: ' + perLetter,
       'new string length: ' + val.length,
       'valSvgViewBox: ' + valSvgViewBox);
 
@@ -46,7 +61,7 @@
       //containerWidth = $elContainer.width();
       containerWidth = $elContainer[0].getBoundingClientRect().width;
 
-    console.log('Before inc loop: svgWidth',svgWidth,
+    ns.debug('Before inc loop: svgWidth',svgWidth,
       'svgTextWidth',svgTextWidth,
       'containerWidth',containerWidth,
       'valSvgViewBox',valSvgViewBox);
@@ -67,10 +82,10 @@
       containerWidth = $elContainer[0].getBoundingClientRect().width;
     }
 
-    console.log('After inc loop of '+ actionInc + ' times: svgWidth',svgWidth, 'svgTextWidth',svgTextWidth, 'containerWidth',containerWidth, 'valSvgViewBox',valSvgViewBox);
+    ns.debug('After inc loop of '+ actionInc + ' times: svgWidth',svgWidth, 'svgTextWidth',svgTextWidth, 'containerWidth',containerWidth, 'valSvgViewBox',valSvgViewBox);
 
     if (actionInc === 0) {
-      console.log('Before dec loop: svgWidth',svgWidth,
+      ns.debug('Before dec loop: svgWidth',svgWidth,
         'svgTextWidth',svgTextWidth,
         'containerWidth',containerWidth,
         'valSvgViewBox',valSvgViewBox);
@@ -85,7 +100,7 @@
         svgTextWidth = $elSvgText[0].getBoundingClientRect().width;
         containerWidth = $elContainer[0].getBoundingClientRect().width;
       }
-      console.log('After dec loop of '+ actionDec + ' times: svgWidth',svgWidth, 'svgTextWidth',svgTextWidth, 'containerWidth',containerWidth, 'valSvgViewBox',valSvgViewBox);
+      ns.debug('After dec loop of '+ actionDec + ' times: svgWidth',svgWidth, 'svgTextWidth',svgTextWidth, 'containerWidth',containerWidth, 'valSvgViewBox',valSvgViewBox);
     }
 
     $elContainer.addClass('lili-text-container-transform');
@@ -94,7 +109,8 @@
 
   $(function() {
 
-    $('.changeText').on('click',ns.changeText);
+    $('.changeText').on('click',ns.cbSelectionText);
+    $('#lili-custom-text').on('change keyup paste', ns.cbCustomText);
 
   });
 
