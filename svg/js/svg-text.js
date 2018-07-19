55464e18c8c1f84dd17ef62fa861e2e9d8e213c2
Li Li <5024766+levonlee@users.noreply.github.com>
2018-07-19 10:50:51 -0400

    svg text remove css transform before resizing

--- svg/js/svg-text.js
+++ svg/js/svg-text.js
@@ -14,6 +14,11 @@
     }
   }
 
+  ns.setViewbox = function($el, $v) {
+    $el.removeAttr('viewBox');
+    $el[0].setAttribute('viewBox', '0 0 ' + $v + ' 19');
+  }
+
   ns.changeText = function(event) {
     var $target = $(event.target),
       oldLength = 6, // 'Fit Me' length = 6
@@ -25,45 +30,44 @@
       $elSvg = $('.lili-text-container svg'),
       $elSvgText = $('.lili-text-container svg text');
 
-    $('.lili-text-container svg text').html(val);
+    $elContainer.removeClass('lili-text-container-transform');
+
+    $elSvgText.text(val); // .html(val) does not work on IE11
+    ns.setViewbox($elSvg,valSvgViewBox);
+
     console.log('per letter length: ' + perLetter,
       'new string length: ' + val.length,
       'valSvgViewBox: ' + valSvgViewBox);
 
-    $elSvg.removeAttr('viewBox');
-    $elSvg[0].setAttribute('viewBox', '0 0 ' + valSvgViewBox + ' 19');
-
     // Can't use jQuery width()
     //var svgWidth = $('.lili-text-container svg').width();
-    var svgWidth = $elSvg[0].getBoundingClientRect().width;
-    var svgTextWidth = $elSvgText[0].getBoundingClientRect().width;
-    //var containerWidth = $elContainer.width();
-    var containerWidth = $elContainer[0].getBoundingClientRect().width;
+    var svgWidth = $elSvg[0].getBoundingClientRect().width,
+      svgTextWidth = $elSvgText[0].getBoundingClientRect().width,
+      //containerWidth = $elContainer.width();
+      containerWidth = $elContainer[0].getBoundingClientRect().width;
 
     console.log('Before inc loop: svgWidth',svgWidth,
       'svgTextWidth',svgTextWidth,
       'containerWidth',containerWidth,
       'valSvgViewBox',valSvgViewBox);
 
-    var actionInc = 0;
-    var actionMax = 10000;
-    var actionDec = 0;
+    var actionInc = 0,
+      actionMax = 1000,
+      actionDec = 0,
+      actionStep = 1;
 
-    while (svgTextWidth > svgWidth && actionInc < actionMax) {
+    while ((svgTextWidth > svgWidth) && (actionInc < actionMax)) {
       ++actionInc;
-      valSvgViewBox = valSvgViewBox + .5;
+      valSvgViewBox = valSvgViewBox + actionStep;
 
-      $elSvg.removeAttr('viewBox');
-      $elSvg[0].setAttribute('viewBox', '0 0 ' + valSvgViewBox + ' 19');
+      ns.setViewbox($elSvg,valSvgViewBox);
+
       svgWidth = $elSvg[0].getBoundingClientRect().width;
       svgTextWidth = $elSvgText[0].getBoundingClientRect().width;
       containerWidth = $elContainer[0].getBoundingClientRect().width;
-
-      console.log('svgWidth',svgWidth,
-        'svgTextWidth',svgTextWidth,
-        'containerWidth',containerWidth,
-        'valSvgViewBox',valSvgViewBox);
     }
+
+    console.log('After inc loop of '+ actionInc + ' times: svgWidth',svgWidth, 'svgTextWidth',svgTextWidth, 'containerWidth',containerWidth, 'valSvgViewBox',valSvgViewBox);
 
     if (actionInc === 0) {
       console.log('Before dec loop: svgWidth',svgWidth,
@@ -71,19 +75,20 @@
         'containerWidth',containerWidth,
         'valSvgViewBox',valSvgViewBox);
 
-      while (svgTextWidth < svgWidth && actionDec < actionMax) {
+      while ((svgTextWidth < svgWidth) && (actionDec < actionMax)) {
         ++actionDec;
-        valSvgViewBox = valSvgViewBox - .5;
+        valSvgViewBox = valSvgViewBox - actionStep;
 
-        $elSvg.removeAttr('viewBox');
-        $elSvg[0].setAttribute('viewBox', '0 0 ' + valSvgViewBox + ' 19');
+        ns.setViewbox($elSvg,valSvgViewBox);
+
         svgWidth = $elSvg[0].getBoundingClientRect().width;
         svgTextWidth = $elSvgText[0].getBoundingClientRect().width;
         containerWidth = $elContainer[0].getBoundingClientRect().width;
-
-        console.log('svgWidth',svgWidth,'svgTextWidth',svgTextWidth,'containerWidth',containerWidth,'valSvgViewBox',valSvgViewBox);
       }
+      console.log('After dec loop of '+ actionDec + ' times: svgWidth',svgWidth, 'svgTextWidth',svgTextWidth, 'containerWidth',containerWidth, 'valSvgViewBox',valSvgViewBox);
     }
+
+    $elContainer.addClass('lili-text-container-transform');
 
   }
 
