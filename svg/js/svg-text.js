a12b7bd7d1c2d07b62176c1b4b539ef2ade38e5d
Li Li <levonlee@gmail.com>
2018-07-19 09:16:21 -0400

    svg text jquery .getBoundingClientRect

--- svg/js/svg-text.js
+++ svg/js/svg-text.js
@@ -20,46 +20,68 @@
       oldSvgViewBox = 45, // <svg viewBox="0 0 45 18">
       perLetter =  oldSvgViewBox / oldLength,
       val = $target.text(),
-      valSvgViewBox = val.length * perLetter;
+      valSvgViewBox = val.length * perLetter,
+      $elContainer = $('.lili-text-container'),
+      $elSvg = $('.lili-text-container svg'),
+      $elSvgText = $('.lili-text-container svg text');
+
     $('.lili-text-container svg text').html(val);
-    console.log('per letter length: ' + perLetter);
-    console.log('new string length: ' + val.length);
-    console.log('valSvgViewBox: ' + valSvgViewBox);
+    console.log('per letter length: ' + perLetter,
+      'new string length: ' + val.length,
+      'valSvgViewBox: ' + valSvgViewBox);
 
-    $('.lili-text-container svg').removeAttr('viewBox');
-    $('.lili-text-container svg')[0].setAttribute('viewBox', '0 0 ' + valSvgViewBox + ' 19');
+    $elSvg.removeAttr('viewBox');
+    $elSvg[0].setAttribute('viewBox', '0 0 ' + valSvgViewBox + ' 19');
 
-    var svgWidth = $('.lili-text-container svg').width();
-    var svgTextWidth = $('.lili-text-container svg text')[0].getBoundingClientRect().width;
-    var containerWidth = $('.lili-text-container').width();
+    // Can't use jQuery width()
+    //var svgWidth = $('.lili-text-container svg').width();
+    var svgWidth = $elSvg[0].getBoundingClientRect().width;
+    var svgTextWidth = $elSvgText[0].getBoundingClientRect().width;
+    //var containerWidth = $elContainer.width();
+    var containerWidth = $elContainer[0].getBoundingClientRect().width;
 
-    console.log('Before inc loop: svgWidth',svgWidth,'svgTextWidth',svgTextWidth,'containerWidth',containerWidth);
+    console.log('Before inc loop: svgWidth',svgWidth,
+      'svgTextWidth',svgTextWidth,
+      'containerWidth',containerWidth,
+      'valSvgViewBox',valSvgViewBox);
 
     var actionInc = 0;
+    var actionMax = 10000;
     var actionDec = 0;
 
-    while (svgTextWidth > svgWidth && actionInc < 100) {
+    while (svgTextWidth > svgWidth && actionInc < actionMax) {
       ++actionInc;
-      ++valSvgViewBox;
-      $('.lili-text-container svg').removeAttr('viewBox');
-      $('.lili-text-container svg')[0].setAttribute('viewBox', '0 0 ' + valSvgViewBox + ' 19');
-      svgWidth = $('.lili-text-container svg').width();
-      svgTextWidth = $('.lili-text-container svg text')[0].getBoundingClientRect().width;
-      containerWidth = $('.lili-text-container').width();
-      console.log('svgWidth',svgWidth,'svgTextWidth',svgTextWidth,'containerWidth',containerWidth);
+      valSvgViewBox = valSvgViewBox + .5;
+
+      $elSvg.removeAttr('viewBox');
+      $elSvg[0].setAttribute('viewBox', '0 0 ' + valSvgViewBox + ' 19');
+      svgWidth = $elSvg[0].getBoundingClientRect().width;
+      svgTextWidth = $elSvgText[0].getBoundingClientRect().width;
+      containerWidth = $elContainer[0].getBoundingClientRect().width;
+
+      console.log('svgWidth',svgWidth,
+        'svgTextWidth',svgTextWidth,
+        'containerWidth',containerWidth,
+        'valSvgViewBox',valSvgViewBox);
     }
 
     if (actionInc === 0) {
-      console.log('Before dec loop: svgWidth',svgWidth,'svgTextWidth',svgTextWidth,'containerWidth',containerWidth);
-      while (svgTextWidth < svgWidth && actionDec < 100) {
+      console.log('Before dec loop: svgWidth',svgWidth,
+        'svgTextWidth',svgTextWidth,
+        'containerWidth',containerWidth,
+        'valSvgViewBox',valSvgViewBox);
+
+      while (svgTextWidth < svgWidth && actionDec < actionMax) {
         ++actionDec;
-        --valSvgViewBox;
-        $('.lili-text-container svg').removeAttr('viewBox');
-        $('.lili-text-container svg')[0].setAttribute('viewBox', '0 0 ' + valSvgViewBox + ' 19');
-        svgWidth = $('.lili-text-container svg').width();
-        svgTextWidth = $('.lili-text-container svg text')[0].getBoundingClientRect().width;
-        containerWidth = $('.lili-text-container').width();
-        console.log('svgWidth',svgWidth,'svgTextWidth',svgTextWidth,'containerWidth',containerWidth);
+        valSvgViewBox = valSvgViewBox - .5;
+
+        $elSvg.removeAttr('viewBox');
+        $elSvg[0].setAttribute('viewBox', '0 0 ' + valSvgViewBox + ' 19');
+        svgWidth = $elSvg[0].getBoundingClientRect().width;
+        svgTextWidth = $elSvgText[0].getBoundingClientRect().width;
+        containerWidth = $elContainer[0].getBoundingClientRect().width;
+
+        console.log('svgWidth',svgWidth,'svgTextWidth',svgTextWidth,'containerWidth',containerWidth,'valSvgViewBox',valSvgViewBox);
       }
     }
 
