f0bee900423311c65c8236512ec0f25220d63872
Li Li <5024766+levonlee@users.noreply.github.com>
2018-08-03 13:05:06 -0400

    tweenmax

--- animation/js/my-tweenmax.js
+++ animation/js/my-tweenmax.js
@@ -4,6 +4,7 @@
 	//basic illustration of TweenMax's repeat, repeatDelay, yoyo and onRepeat
 var box = document.getElementById("greenBox"),
 	box2 = document.getElementById("greenBox2"),
+	box3 = document.getElementById("greenBox3"),
 	count = 0,
 	tween,
 tween2;
@@ -16,3 +17,12 @@ function onRepeat() {
 	box.innerHTML = count;
 	TweenLite.set(box, {backgroundColor:"hsl(" + Math.random() * 255 + ", 90%, 60%)"});
 }
+
+box3.addEventListener('mouseenter', function(event) {
+	TweenLite.killTweensOf(box3);
+	TweenLite.to(box3, 1, {css:'background-color:red', ease:Sine.easeOut});
+});
+box3.addEventListener("mouseleave", function(event) {
+	TweenLite.killTweensOf(box3);
+	TweenLite.to(box3, 1, {css:'background-color:#6fb936', ease:Sine.easeOut});
+});
