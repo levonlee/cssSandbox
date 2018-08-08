6631c2224024fb8e60055afd32c350afb3d40525
Li Li <levonlee@gmail.com>
2018-08-08 19:30:20 -0400

    test token

--- pwa/service-worker.js
+++ pwa/service-worker.js
@@ -76,6 +76,7 @@ self.addEventListener('fetch', function(e) {
 	//console.log('[Service Worker] Fetch', e.request.url);
 	var dataUrl = 'https://query.yahooapis.com/v1/public/yql';
 	if (e.request.url.indexOf(dataUrl) > -1) {
+		
     /*
      * When the request URL contains dataUrl, the app is asking for fresh
      * weather data. In this case, the service worker always goes to the
